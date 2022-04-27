6d3dd4df8639fe5d6933a2eab4500f8aa30f40e7
Endreu Benites <endreu.matteusrb@gmail.com>
2022-04-27 07:21:39 -0300

    Criada tela de Profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,87 @@
 import React from 'react';
+import RandomCalendar from './../../components/RandomCalendar';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Main,
+  LeftSide,
+  RightSide,
+  Repos,
+  CalendarHeading,
+  RepoIcon,
+  Tab,
+} from './styles';
+
+import ProfileData from './../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
 
 const Profile: React.FC = () => {
-  return <div />;
+  const TabContent = () => {
+    return (
+      <div className='content'>
+        <RepoIcon />
+        <span className='label'>Repositories</span>
+        <span className='number'>26</span>
+      </div>
+    );
+  };
+
+  return (
+    <Container>
+      <Tab className='desktop'>
+        <div className='wrapper'>
+          <span className='offset' />
+          <TabContent />
+        </div>
+
+        <span className='line'></span>
+      </Tab>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={'endreumrb'}
+            name={'Endreu Benites'}
+            avatarUrl={'https://avatars.githubusercontent.com/u/33642653?v=4'}
+            followers={887}
+            following={7}
+            company={'HNSG'}
+            location={'Curitiba, Brazil'}
+            email={'endreu.matteusrb@gmail.com'}
+            blog={'https://www.linkedin.com/in/endreu-benites/'}
+          />
+        </LeftSide>
+        <RightSide>
+          <Tab className='mobile'>
+            <TabContent />
+            <span className='line'></span>
+          </Tab>
+          <Repos>
+            <h2>Random repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map((n) => (
+                <RepoCard
+                  key={n}
+                  username={'endreumrb'}
+                  reponame={' BlueRidge-ReactJS'}
+                  description={'Um repos padrão'}
+                  language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+
+          <CalendarHeading>
+            Random calendar (do not respresent actual data)
+          </CalendarHeading>
+
+          <RandomCalendar />
+        </RightSide>
+      </Main>
+    </Container>
+  );
 };
 
 export default Profile;
