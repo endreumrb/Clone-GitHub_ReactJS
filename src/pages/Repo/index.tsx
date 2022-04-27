a2df3101c5536a859e5e3e4ea735f472a712c6c8
Endreu Benites <endreu.matteusrb@gmail.com>
2022-04-27 20:00:47 -0300

    Criada tela de Repositorio

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrump,
+  Stats,
+  LinkButton,
+  RepoIcon,
+  StarIcon,
+  ForkIcon,
+  GithubIcon,
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrump>
+        <RepoIcon />
+        <Link className={'username'} to={'/endreumrb'}>
+          Endreumrb
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/endreumrb/React'}>
+          Teste
+        </Link>
+      </Breadcrump>
+
+      <p>Qulaqeq descrição</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/endreumrb'}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
 export default Repo;
