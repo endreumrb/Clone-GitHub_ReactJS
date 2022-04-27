d682c783a9b116564698c2773bb794d7f30ad3aa
Endreu Benites <endreu.matteusrb@gmail.com>
2022-04-26 21:37:40 -0300

    Criando estrutura de pastas e arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
 
 function App() {
-  return <h1>AAA</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Profile />} />
+        <Route path='/:username' element={<Profile />} />
+        <Route path='/:username/:reponame' element={<Repo />} />
+      </Routes>
+      {/* {<Footer />} */}
+
+      <GlobalStyles />
+    </BrowserRouter>
+  );
 }
 
 export default App;
